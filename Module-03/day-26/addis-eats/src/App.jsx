5b11cc4bcc01b778ad1014eebcb36859285cc2f7
Mussie_G <mosabyg@gmail.com>
2026-08-28 13:52:10 -0700

    Add Card component using children prop

--- Module-03/day-26/addis-eats/src/App.jsx
+++ Module-03/day-26/addis-eats/src/App.jsx
@@ -1,11 +1,18 @@
 import Header from "./component/Header/Header.jsx";
 import Main from "./component/Main/Main.jsx";
 import Footer from "./component/Footer/Footer.jsx";
+import Card from "./component/Card/Card.jsx";
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <Card>
+        <h2>Addis Eats Menu</h2>
+        <p>Welcome to our delicious Ethiopian menu.</p>
+      </Card>
+
       <Main />
       <Footer />
     </div>
